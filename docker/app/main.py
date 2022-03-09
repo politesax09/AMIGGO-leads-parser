f72045f7a7279eca3e3eb0d9e0ad559cc8549828
Ubuntu <j.adrian.mazueco@gmial.com>
2022-03-09 02:22:51 +0000

    refresco token api

--- docker/app/main.py
+++ docker/app/main.py
@@ -31,3 +31,9 @@ def lastemail():
             'producto': producto,
             'url': url}
 
+@app.get('/last-email2')
+def lastemail2():
+    return {'fecha':'2021-11-26 08:02:09',
+            'codigo': '6531787',
+            'producto': 'Hogar',
+            'url':'https://apps.walmeric.com/clients/custom/mapfre/rm/20211129/?clientHash=28945f94d414949bf95fb2e6109142cfef1fb2a78114c2c536a36bf5a65b953abe4cb254380fee21da1e87e8c82a173c2256c0940ada1f402a8e36cb8005bad3c42fab8ae8e2f8d511d907204b5b78f7e3c6ad2802809f6f278c6e1ac928d48be43f94c9413d24187f5e8c03a2ed269d&leadId=425d1d1c409dc616a325063a4e7721e565266595&ownerId=75bfb58ff1b0a0197a127f733170910b'}
